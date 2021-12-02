b7c833d96a2542dcec039ea2e3722f3ec56c8919
rchepu2 <rchepu2@uic.edu>
2021-12-02 02:30:30 -0600

    fix user preferences bug

--- controllers/preference_controller.js
+++ controllers/preference_controller.js
@@ -20,8 +20,14 @@ module.exports.updatePage = async function (req, res) {
         let user = {};
         user.username = res.locals.user.username;
         let sourceIds = await userPreference.getSourceIds(res.locals.user.username);
+        console.log("sourceIds");
+        console.log(sourceIds);
         let selectedSources = await sourceObject.getAllSources(sourceIds);
+        console.log("selectedSources");
+        console.log(selectedSources);
         let sources = await sourceObject.getAll();
+        console.log("sources");
+        console.log(sources);
         let data = {};
         data.sources = sources;
         data.selectedSources = selectedSources;
@@ -34,7 +40,10 @@ module.exports.updatePage = async function (req, res) {
 
 module.exports.updatePreferences = async function (req, res) {
     try {
-        await userPreference.updatePreferences(req.body.preference, req.body.username);
+        if(typeof req.body.preference == 'string')
+            await userPreference.updatePreferences([parseInt(req.body.preference)], req.body.username);
+        else
+            await userPreference.updatePreferences(req.body.preference, req.body.username);
         return res.redirect('/users/home');
     }
     catch (err) {
