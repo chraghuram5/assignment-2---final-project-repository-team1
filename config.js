8ed451c3f602b826075b936cad0c8f38e4cbbe56
rchepu2 <rchepu2@uic.edu>
2021-12-02 18:46:15 -0600

    remove crypto-js library

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
 const dotenv = require('dotenv');
 const path = require('path');
-var CryptoJS = require("crypto-js");
 
 dotenv.config({
     path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
 });
-
-//var bytes  = CryptoJS.AES.decrypt(process.env.apiKey, 'one-news-app');
-//var apiKey = bytes.toString(CryptoJS.enc.Utf8);
 
 console.log(process.env.NODE_ENV);
 
