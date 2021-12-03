da05dd3b75c0437d31a6595c898f8b781b7a4bdb
rchepu2 <rchepu2@uic.edu>
2021-12-02 18:27:19 -0600

    revert crypto changes

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ dotenv.config({
     path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
 });
 
-var bytes  = CryptoJS.AES.decrypt(process.env.apiKey, 'one-news-app');
-var apiKey = bytes.toString(CryptoJS.enc.Utf8);
+//var bytes  = CryptoJS.AES.decrypt(process.env.apiKey, 'one-news-app');
+//var apiKey = bytes.toString(CryptoJS.enc.Utf8);
 
 console.log(process.env.NODE_ENV);
 
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV : process.env.NODE_ENV || 'development',
-    apiKey : apiKey || '459f6dffd625423485f6ba2c77bea075',
+    apiKey : process.env.apiKey || '459f6dffd625423485f6ba2c77bea075',
     dbFileName : process.env.dbFileName || 'test.sqlite'
 }
--- utilities/cryptoHelper.js
+++ utilities/cryptoHelper.js
@@ -1,10 +1,10 @@
-var CryptoJS = require("crypto-js");
+// var CryptoJS = require("crypto-js");
 
-// Encrypt
-var ciphertext = CryptoJS.AES.encrypt('', 'one-news-app').toString();
-console.log(ciphertext);
-// Decrypt
-var bytes  = CryptoJS.AES.decrypt(ciphertext, 'one-news-app');
-var originalText = bytes.toString(CryptoJS.enc.Utf8);
+// // Encrypt
+// var ciphertext = CryptoJS.AES.encrypt('', 'one-news-app').toString();
+// console.log(ciphertext);
+// // Decrypt
+// var bytes  = CryptoJS.AES.decrypt(ciphertext, 'one-news-app');
+// var originalText = bytes.toString(CryptoJS.enc.Utf8);
 
-console.log(originalText); // 'my message'
+// console.log(originalText); // 'my message'
